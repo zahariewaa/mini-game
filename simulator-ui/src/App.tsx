c25ecc78ae6b3924884842716d9501ceb7b3545a
Anastasia Kichukova <akichukova@evolution>
2024-11-24 22:24:02 +0200

    removed counter data

--- simulator-ui/src/App.tsx
+++ simulator-ui/src/App.tsx
@@ -6,18 +6,10 @@ import { Button } from '@/components/ui/button';
 import { Progress } from "@/components/ui/progress"
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  // Event handler for button clicks
-  const handleButtonClick = () => {
-    setCount((prevCount) => prevCount + 1);
-  };
-
   return (
     <div>
       <Button variant="outline">Button</Button>
       <Progress value={33} />
-
     </div>
   );
 }
